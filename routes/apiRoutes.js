bb433ab9eceb992ab3c2b209d8f11fbed8469484
August Johnson <gusjohnson98@gmail.com>
2019-03-23 12:06:54 -0500

    lint auto fixables

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,13 +11,15 @@ module.exports = function(app) {
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
-      res.json(  dbExample  );
+      res.json(dbExample);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
