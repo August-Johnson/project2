1dc5b2d36f90cc91e2b956cc9e2711a18237a1ca
August Johnson <gusjohnson98@gmail.com>
2019-03-23 11:48:35 -0500

    lint error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+      res.json(  dbExample  );
     });
   });
 
