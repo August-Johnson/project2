37789c4f949a58a271bf2074f4ba8070b0421cbf
August Johnson <gusjohnson98@gmail.com>
2019-03-25 21:14:03 -0500

    added minimum length to user's name

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            len: [1]
         },
         username: {
             type: DataTypes.STRING,
