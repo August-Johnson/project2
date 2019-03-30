d5e613fb989488dad4a29042471929fb08a35ea7
Sindy <sindy.gebhard@gmail.com>
2019-03-30 13:42:44 -0500

    error messages validation
    error validations done

--- public/js/site.js
+++ public/js/site.js
@@ -91,13 +91,17 @@ $("#loginAccount").on("click", function (event) {
     var username = $("#usernameLogin").val().trim();
     var password = $("#passwordLogin").val().trim();
 
-    // Checking the username field
+    // Checking the username field - No username entered
     if (username === "" || username === undefined || username === null) {
-        return alert("Username field is empty!");
+        $("#usernameLogin").attr('class', 'input is-danger');
+        $("#enterUserLogin").css('display', 'inline');
+        $('#errorUserLogin').css('visibility', 'visible');
     }
-    // Checking the password field
+    // Checking the password field - No password entered
     else if (password === "" || password === undefined || password === null) {
-        return alert("Password field is empty!");
+        $("#passwordLogin").attr('class', 'input is-danger');
+        $("#enterPassLogin").css('display', 'inline');
+        $('#errorIconPass').css('visibility', 'visible');
     }
     else {
         // If the login info is valid, create an object to send in the post request, containing the login data.
@@ -122,21 +126,41 @@ $("#createAccount").on("click", function (event) {
     var userImage = $("#imageLinkCreate").val().trim();
     var goal = $("#goalCategory").val();
 
-    // Checking the username field
+    // Checking the username field - Enter Username (Blank field)
     if (username === "" || username === undefined || username === null) {
-        return alert("Please enter a username!");
+        $("#usernameCreate").attr('class', 'input is-danger');
+        $("#enterNewUser").css('display', 'inline');
+        $("#errorUserCreate").css('visibility', 'visible');
     }
-    // Checking the password field
+    // Checking the username field - Username Length Error
+    if (username.length > 20 || username.length < 5) {
+        $("#usernameCreate").attr('class', 'input is-danger');
+        $("#userNewLength").css('display', 'inline');
+        $("#errorUserCreate").css('visibility', 'visible');
+    }
+    // Checking the password field - Enter Password (Blank field)
     else if (password === "" || password === undefined || password === null) {
-        return alert("Please enter a password!");
+        $("#passwordCreate").attr('class', 'input is-danger');
+        $("#enterNewPass").css('display', 'inline');
+        $("#passErrorCreate").css('visibility', 'visible');
+    }
+    // Checking the password field - Must be 5 characters
+    else if (password.length < 5) {
+        $("#passwordCreate").attr('class', 'input is-danger');
+        $("#passNewLength").css('display', 'inline');
+        $("#passErrorCreate").css('visibility', 'visible');
     }
     // Checking if the confirm password field matches the password field
     else if (password !== passwordConfirm) {
-        return alert("Your passwords do not match!");
+        $("#passwordConfirm").attr('class', 'input is-danger');
+        $("#passNoMatch").css('display', 'inline');
+        $("#passErrorCreate2").css('visibility', 'visible');
     }
     // Checking if the user provided an image link
     else if (userImage === "" || userImage === undefined || userImage === null) {
-        return alert("Please provide an image URL!");
+        $("#imageLinkCreate").attr('class', 'input is-danger');
+        $("#imgNone").css('display', 'inline');
+        $("#imgError").css('visibility', 'visible');
     }
     else {
         // If all of the fields are valid, creates an object that will be passed in the post request.
