0d721fe75713adfc968a7c85723081947780554d
czops <37029225+czops@users.noreply.github.com>
2019-03-30 13:45:08 -0500

    creating jQuery to append boxes

--- public/js/userGoals.js
+++ public/js/userGoals.js
@@ -1,46 +1,46 @@
 $(document).ready(function () {
     alert("I ran first!");
 
-    $("#createAccount").on("click", function (event) {
-        event.preventDefault();
 
-        //
-        var username = $("#usernameCreate").val().trim();
-        var password = $("#passwordCreate").val().trim();
-        var passwordConfirm = $("#passwordConfirm").val().trim();
-        var userImage = $("#imageLinkCreate").val().trim();
-        var goal = $("#goalCategory").val();
+    var goalsList = $("#goalsList");
 
-        // Checking the username field
-        if (username === "" || username === undefined || username === null) {
-            return alert("Please enter a username!");
-        }
-        // Checking the password field
-        else if (password === "" || password === undefined || password === null) {
-            return alert("Please enter a password!");
-        }
-        // Checking if the confirm password field matches the password field
-        else if (password !== passwordConfirm) {
-            return alert("Your passwords do not match!");
-        }
-        // Checking if the user provided an image link
-        else if (userImage === "" || userImage === undefined || userImage === null) {
-            return alert("Please provide an image URL!");
-        }
-        else {
-            // If all of the fields are valid, creates an object that will be passed in the post request.
-            // Object holds all of the user's create account info
-            var userCreateData = {
-                usernameData: username,
-                passwordData: password,
-                userImageData: userImage,
-                goalData: goal
-            }
+    $.ajax("/userGoals", {
+        type: "GET",
+        data: userLoginData
+    }).then(function (data) {
+        $()
 
-            $.post("/newUser", { data: userCreateData }).then(function (results) {
-                //
-            });
-        }
     });
+
+
+    function createNewRow(post) {
+        
+    };
+
+    function populateUserGoalsTable() {
+        goalsList.empty();
+        var postsToAdd = [];
+        for (var i = 0; i < posts.length; i++) {
+          postsToAdd.push(createNewRow(posts[i]));
+        }
+        goalsList.append(postsToAdd);
+      }
+
+    function getPosts(category) {
+        var categoryString = category || "";
+        if (categoryString) {
+          categoryString = "/category/" + categoryString;
+        }
+        $.get("/api/posts" + categoryString, function(data) {
+          console.log("Posts", data);
+          posts = data;
+          if (!posts || !posts.length) {
+            displayEmpty();
+          }
+          else {
+            populateUserGoalsTable();
+          }
+        });
+      }
 
 });
