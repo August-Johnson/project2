c8fca90ec40f917bb1ea9ad1cb6341ce74f8657d
czops <37029225+czops@users.noreply.github.com>
2019-03-30 19:50:26 -0500

    semicolon debug

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -25,8 +25,6 @@ module.exports = function (app) {
     });
   });
 
-}
-
 // // Load example page and pass in an example by id
 // app.get("/example/:id", function (req, res) {
 //   db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
