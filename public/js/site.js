3bcb8520860b15eaf12d93843589e7d117a589af
Sindy <sindy.gebhard@gmail.com>
2019-04-01 20:51:53 -0500

    quick visual edits
    get rid of category on user.html and working hamburger menu

--- public/js/site.js
+++ public/js/site.js
@@ -135,7 +135,12 @@ $(document).ready(function () {
             });
         };
     });
+    $(".navbar-burger").click(function() {
 
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
     //Message Board Posting
     // $("#postButton").on("click", function(event) {
     //     event.preventDefault();
